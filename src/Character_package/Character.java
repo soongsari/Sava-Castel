6bc30dfaa94192894747944cdde9bf0ec5592390
JiHye <sonjeehe@gmail.com>
2015-08-15 16:08:36 +0900

    uplode

---
+++ src/Character_package/Character.java
@@ -0,0 +1,20 @@
+package Character_package;
+
+import java.awt.Graphics;
+
+public abstract class Character {
+	
+	int HP;
+	int SPEED;
+	int STR;
+	boolean die;
+	int x,y;
+	boolean type;
+	int image;
+	
+	public abstract void Attack();
+	public abstract void Move();
+	public abstract void Damaged();
+	public abstract void Meet();
+	public abstract void Paint(Graphics g);
+}
